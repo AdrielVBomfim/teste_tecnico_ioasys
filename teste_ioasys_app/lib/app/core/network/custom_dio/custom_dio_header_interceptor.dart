ce0b8d0817b64b19b32b533507b80f84b08c890c
Adriel Venancio Bomfim <adrielvbomfim@gmail.com>
2021-04-18 07:52:22 -0300

    chore: tira placeholders dos cabeçalhos do Dio customizado

--- teste_ioasys_app/lib/app/core/network/custom_dio/custom_dio_header_interceptor.dart
+++ teste_ioasys_app/lib/app/core/network/custom_dio/custom_dio_header_interceptor.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
-
 import 'package:dio/dio.dart';
+import 'package:teste_ioasys_app/app/common/headers/domain/usecases/ler_headers_usecase.dart';
 
 class CustomDioHeaderInterceptor extends InterceptorsWrapper {
   CustomDioHeaderInterceptor();
 
   @override
   Future<void> onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    final headers = await LerHeadersUsecase()();
+
     options.headers[HttpHeaders.contentTypeHeader] = 'application/json';
-    options.headers["uid"] = 'placeholder';
-    options.headers["access-token"] = 'placeholder';
-    options.headers["client"] = 'placeholder';
+    options.headers['uid'] = headers['uid'];
+    options.headers['access-token'] = headers['access-token'];
+    options.headers['client'] = headers['client'];
     handler.next(options);
   }
 }
